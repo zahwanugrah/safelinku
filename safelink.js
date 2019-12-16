95f1728e6618004f3cef4ff3a0898a234898fcef
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2019-12-16 11:18:24 +0800

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -21,11 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="7-diet-secrets-of-stars.html"}
-if(x=="2"){xxx="multivitamins-ticket-to-daily-health.html"}
-if(x=="3"){xxx="benefits-of-whey-protein.html"}
-if(x=="4"){xxx="quick-tips-to-boost-your-metabolism.html"}
-if(x=="5"){xxx="the-importance-of-working-your-core.html"}a_to_ve[i].href="https://pw.safelink-zhangzi.com/2019/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="http-injector.html"}
+if(x=="2"){xxx="http-injector-lite.html"}
+if(x=="3"){xxx="http-custom.html"}
+if(x=="4"){xxx="openvpn-connect-fast-safe-ssl-vpn-client.html"}a_to_ve[i].href="https://www.masraden.com/2019/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
