a67d3b2576c9781a22ad344690739c14bdb4f63e
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2019-12-17 09:40:40 +0800

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -19,12 +19,15 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*7)+ 1);
 var xxx=null;
 if(x=="1"){xxx="http-injector.html"}
 if(x=="2"){xxx="http-injector-lite.html"}
 if(x=="3"){xxx="http-custom.html"}
-if(x=="4"){xxx="openvpn-connect-fast-safe-ssl-vpn-client.html"}a_to_ve[i].href="https://www.masraden.com/2019/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="openvpn-connect-fast-safe-ssl-vpn-client.html"}
+if(x=="5"){xxx="4-tips-to-consider-when-buying-best.html"}
+if(x=="6"){xxx="best-laptops-under-600-2019.html"}
+if(x=="7"){xxx="review-on-acer-aspire-e15-e5-576g-5762.html"}a_to_ve[i].href="https://www.masraden.com/2019/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
